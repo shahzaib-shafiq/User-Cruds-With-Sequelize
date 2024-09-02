2e5b426cc3d5502a7d22c092551df251637b1683
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-09-02 11:21:30 +0500

    Implemented Departmnt Validations

--- src/Controller/DepartmentController.js
+++ src/Controller/DepartmentController.js
@@ -1,11 +1,21 @@
+const { CreateDepartment } = require("../utils/DepartmentValidations");
 const Department = require("../Model/DepartmentModel");
 exports.AddDepartment = async (req, res) => {
-  const Departmentdata = req.body;
-  const department = await Department.create(Departmentdata);
+  try {
+    const Departmentdata = req.body;
 
-  if (department) {
-    res.status(200).json({ message: "Data Added to DB" });
-  } else {
-    res.status(401).json({ message: "Failed to Add Data to DB" });
+    const ValidateData = CreateDepartment(req.body);
+    if (!ValidateData) {
+      return res.status(400).json({ message: "Validation failed" });
+    }
+    const department = await Department.create(Departmentdata);
+
+    if (department) {
+      return res.status(200).json({ message: "Data Added to DB" });
+    } else {
+      return res.status(401).json({ message: "Failed to Add Data to DB" });
+    }
+  } catch (error) {
+    return res.status(401).json({ message: error });
   }
 };
